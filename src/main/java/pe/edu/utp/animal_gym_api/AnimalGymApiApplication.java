95a7ff71f8180b98cbc12beb71e84959c650c419
aomine <aomine1745@gmail.com>
2026-05-31 11:34:49 -0500

    agregar boilerplate de spring boot

---
+++ src/main/java/pe/edu/utp/animal_gym_api/AnimalGymApiApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.utp.animal_gym_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalGymApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimalGymApiApplication.class, args);
+	}
+
+}
