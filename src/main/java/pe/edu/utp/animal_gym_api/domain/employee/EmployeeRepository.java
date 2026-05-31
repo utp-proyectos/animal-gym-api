61ee980f6c221b87015cbc8b7ac573275ce7be63
leo <leonardoelbaneado@gmail.com>
2026-05-31 15:49:17 -0500

    feat: repositorio creado

--- src/main/java/pe/edu/utp/animal_gym_api/domain/employee/EmployeeRepository.java
+++ src/main/java/pe/edu/utp/animal_gym_api/domain/employee/EmployeeRepository.java
@@ -1,17 +1,12 @@
 package pe.edu.utp.animal_gym_api.domain.employee;
 
-import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-	@Query("SELECT e FROM Empleado e WHERE LOWER(e.nombre) LIKE LOWER(CONCAT('%', :nombre, '%'))")
-	List<Employee> buscarPorNombre(@Param("nombre") String nombre);
 
 	Optional<Employee> findByDni(String dni);
 }
