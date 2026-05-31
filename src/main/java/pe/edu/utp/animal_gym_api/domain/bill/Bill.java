e099e3c3ad31be002991b6a8ca6b6d554de87715
leo <leonardoelbaneado@gmail.com>
2026-05-31 17:51:31 -0500

    feat: agregar entidad

--- src/main/java/pe/edu/utp/animal_gym_api/domain/bill/Bill.java
+++ src/main/java/pe/edu/utp/animal_gym_api/domain/bill/Bill.java
@@ -7,11 +7,13 @@ import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -22,6 +24,8 @@ import pe.edu.utp.animal_gym_api.domain.partner.Partner;
 @AllArgsConstructor
 @NoArgsConstructor
 
+@Entity
+@Table(name = "bills")
 public class Bill {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
