b47a42e267fd4c2f46c9ca459c73b1e3d44d06b5
leo <leonardoelbaneado@gmail.com>
2026-05-31 15:21:25 -0500

    feat: agregar modelo empleado

---
+++ src/main/java/pe/edu/utp/animal_gym_api/domain/employee/Employee.java
@@ -0,0 +1,34 @@
+package pe.edu.utp.animal_gym_api.domain.employee;
+
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import pe.edu.utp.animal_gym_api.domain.person.Person;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+@Entity
+@Table(name = "employees")
+@OnDelete(action = OnDeleteAction.CASCADE)
+public class Employee extends Person {
+
+	@Column(name = "image", nullable = false)
+	private String image;
+
+	@Column(name = "salary", nullable = false)
+	private double salary;
+
+	@Column(name = "contract_type", nullable = false, length = 50)
+	private String contractType;
+
+	@Column(name = "specialty", length = 60)
+	private String specialty;
+}
