03babc903702961a8ce22e17fca9c6898ae72d91
leo <leonardoelbaneado@gmail.com>
2026-05-31 21:08:00 -0500

    feat: crear mapper boleta

---
+++ src/main/java/pe/edu/utp/animal_gym_api/domain/bill/BoletaMapper.java
@@ -0,0 +1,22 @@
+package pe.edu.utp.animal_gym_api.domain.bill;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import pe.edu.utp.animal_gym_api.domain.bill.dto.BillRequestDTO;
+import pe.edu.utp.animal_gym_api.domain.bill.dto.BillResponseDTO;
+
+@Mapper(componentModel = "spring")
+public interface BoletaMapper {
+	@Mapping(target = "id", ignore = true)
+	@Mapping(target = "partner", ignore = true)
+	@Mapping(target = "employee", ignore = true)
+	Bill toEntity(BillRequestDTO dto);
+
+	@Mapping(target = "employeeFirstName", source = "bill.employee.firstName")
+	@Mapping(target = "employeeLastName", source = "bill.employee.lastName")
+	@Mapping(target = "partnerFirstName", source = "bill.partner.firstName")
+	@Mapping(target = "partnerLastName", source = "bill.partner.lastName")
+	@Mapping(target = "membershipName", source = "bill.partner.membership.name")
+	BillResponseDTO toResponseDto(Bill bill);
+}
