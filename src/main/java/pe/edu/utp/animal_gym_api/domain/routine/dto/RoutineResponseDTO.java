2ad6321300f4e849eeaa772a315568958e01e5b3
Jhordan <calixto4211@gmail.com>
2026-06-02 10:27:58 -0500

    fix: arreglar mapper de rutina y sus dto

--- src/main/java/pe/edu/utp/animal_gym_api/domain/routine/dto/RoutineResponseDTO.java
+++ src/main/java/pe/edu/utp/animal_gym_api/domain/routine/dto/RoutineResponseDTO.java
@@ -1,16 +1,24 @@
 package pe.edu.utp.animal_gym_api.domain.routine.dto;
 
 import java.time.LocalDate;
+import java.util.List;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import pe.edu.utp.animal_gym_api.domain.employee.dto.EmployeeResponseDTO;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class RoutineSummaryDTO {
+public class RoutineResponseDTO {
+	private Long id;
 	private String name;
+	private String description;
+	private String goal;
 	private LocalDate startDate;
 	private LocalDate endDate;
+
+	private EmployeeResponseDTO employee;
+	private List<RoutineDetailResponseDTO> details;
 }
