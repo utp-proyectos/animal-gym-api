d5d4e7b99f2373aa9d743448c38a7042aa9a241a
Mishell <indexclever@gmail.com>
2026-06-02 16:03:59 -0500

    fix: corregir mapper membership

--- src/main/java/pe/edu/utp/animal_gym_api/domain/membership/mapper/MembershipMapper.java
+++ src/main/java/pe/edu/utp/animal_gym_api/domain/membership/mapper/MembershipMapper.java
@@ -1,6 +1,7 @@
 package pe.edu.utp.animal_gym_api.domain.membership.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 
 import pe.edu.utp.animal_gym_api.domain.membership.Membership;
@@ -10,9 +11,19 @@ import pe.edu.utp.animal_gym_api.domain.membership.dto.MembershipResponseDTO;
 @Mapper(componentModel = "spring")
 public interface MembershipMapper {
 
+	@Mapping(target = "id", ignore = true)
 	Membership toEntity(MembershipRequestDTO requestDTO);
 
+	@Mapping(target = "active", ignore = true)
+	@Mapping(target = "expired", ignore = true)
+	@Mapping(target = "remainingDays", ignore = true)
+	@Mapping(target = "enrolledMembers", ignore = true)
 	MembershipResponseDTO toResponseDTO(Membership membership);
 
+	@Mapping(target = "id", ignore = true)
+	@Mapping(target = "active", ignore = true)
+	@Mapping(target = "expired", ignore = true)
+	@Mapping(target = "remainingDays", ignore = true)
+	@Mapping(target = "enrolledMembers", ignore = true)
 	void updateEntityFromDTO(MembershipRequestDTO requestDTO, @MappingTarget Membership membership);
 }
