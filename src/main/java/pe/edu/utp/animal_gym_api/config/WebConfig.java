1776b53599a0e7c29630bac25c78d06cf03943a7
aomine <aomine1745@gmail.com>
2026-06-03 12:36:41 -0500

    feat: agregar configuracion de recursos

---
+++ src/main/java/pe/edu/utp/animal_gym_api/config/WebConfig.java
@@ -0,0 +1,14 @@
+package pe.edu.utp.animal_gym_api.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/storage/**")
+				.addResourceLocations("file:/storage/");
+	}
+}
