5e07a520131f8eaaaeefc9de78f72c79968469b7
Mishell <indexclever@gmail.com>
2026-06-03 01:21:08 -0500

    feat: agregar mapper partner

--- src/main/java/pe/edu/utp/animal_gym_api/domain/partner/mapper/PartnerMapper.java
+++ src/main/java/pe/edu/utp/animal_gym_api/domain/partner/mapper/PartnerMapper.java
@@ -7,23 +7,25 @@ import org.mapstruct.MappingTarget;
 import pe.edu.utp.animal_gym_api.domain.partner.Partner;
 import pe.edu.utp.animal_gym_api.domain.partner.dto.PartnerRequestDTO;
 import pe.edu.utp.animal_gym_api.domain.partner.dto.PartnerResponseDTO;
+import pe.edu.utp.animal_gym_api.domain.partner.dto.PartnerDetailDTO;
 
-@Mapper(componentModel = "spring")
+@Mapper(
+    componentModel = "spring",
+    unmappedTargetPolicy = org.mapstruct.ReportingPolicy.IGNORE
+)
 public interface PartnerMapper {
 
-	@Mapping(target = "firstName", source = "name")
-	@Mapping(target = "phoneNumber", source = "phone")
-	@Mapping(target = "hireDate", source = "joinDate")
-	@Mapping(target = "password", ignore = true)
-	@Mapping(target = "role", ignore = true)
-	@Mapping(target = "membership", ignore = true)
-	Partner toEntity(PartnerRequestDTO requestDTO);
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "membership", ignore = true)
+    @Mapping(target = "routines", ignore = true)
+    Partner toEntity(PartnerRequestDTO requestDTO);
 
-	@Mapping(target = "membershipId", source = "membership.id")
-	@Mapping(target = "membershipName", source = "membership.name")
-	PartnerResponseDTO toResponseDTO(Partner partner);
+    @Mapping(target = "membershipId", source = "membership.id")
+    @Mapping(target = "membershipName", source = "membership.name")
+    PartnerResponseDTO toResponseDTO(Partner partner);
 
-	@Mapping(target = "password", ignore = true)
-	@Mapping(target = "role", ignore = true)
-	void updateEntityFromDTO(PartnerRequestDTO requestDTO, @MappingTarget Partner partner);
+    @Mapping(target = "membershipId", source = "membership.id")
+    @Mapping(target = "membershipName", source = "membership.name")
+    @Mapping(target = "routines", source = "routines")
+    PartnerDetailDTO toDetailDTO(Partner partner);
 }
