1c0f64df94c8faab67b04c14cf88d3abf86d580b
leo <leonardoelbaneado@gmail.com>
2026-06-08 21:29:37 -0500

    fix: quitar importanciones inncesarias y añadir campo dni

--- src/main/java/pe/edu/utp/animal_gym_api/domain/employee/dto/EmployeeResponseDTO.java
+++ src/main/java/pe/edu/utp/animal_gym_api/domain/employee/dto/EmployeeResponseDTO.java
@@ -14,6 +14,6 @@ public class EmployeeResponseDTO {
 	private String firstName;
 	private String lastName;
 	private String avatar;
-	private Role role;
 	private String dni;
+	private Role role;
 }
