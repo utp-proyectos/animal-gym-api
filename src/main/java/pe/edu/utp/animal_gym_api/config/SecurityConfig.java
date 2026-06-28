c665e6c0117bc945398e35f5ee350e7c7158f921
leo <leonardoelbaneado@gmail.com>
2026-06-27 23:49:01 -0500

    feat : integrar peticiones al landing

--- src/main/java/pe/edu/utp/animal_gym_api/config/SecurityConfig.java
+++ src/main/java/pe/edu/utp/animal_gym_api/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -43,6 +44,9 @@ public class SecurityConfig {
 				.authorizeHttpRequests(auth -> auth
 						.requestMatchers("/api/auth/me").authenticated()
 						.requestMatchers("/api/auth/**").permitAll().requestMatchers("/storage/**").permitAll()
+						.requestMatchers("/api/sessions").permitAll()
+						.requestMatchers("/api/memberships").permitAll()
+						.requestMatchers(HttpMethod.POST, "/api/partners").permitAll()
 						.anyRequest().authenticated())
 
 				.exceptionHandling(ex -> ex
