78d613c8423e4d8be60d16e3fd33f750ac9c256d
aomine <aomine1745@gmail.com>
2026-07-01 03:21:54 -0500

    test: probar error de compilacion en ci

--- src/main/java/pe/edu/utp/animal_gym_api/AnimalGymApiApplication.java
+++ src/main/java/pe/edu/utp/animal_gym_api/AnimalGymApiApplication.java
@@ -1,8 +1,5 @@
 package pe.edu.utp.animal_gym_api;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class AnimalGymApiApplication {
 
