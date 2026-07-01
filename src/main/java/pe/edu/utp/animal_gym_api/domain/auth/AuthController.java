3d6f33a68e4ad50e74ffed71c28d8dfd7c29f6df
aomine <aomine1745@gmail.com>
2026-07-01 02:06:24 -0500

    fix: cambiar autowired por final en los archivos del dominio de auth

--- src/main/java/pe/edu/utp/animal_gym_api/domain/auth/AuthController.java
+++ src/main/java/pe/edu/utp/animal_gym_api/domain/auth/AuthController.java
@@ -1,6 +1,5 @@
 package pe.edu.utp.animal_gym_api.domain.auth;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -11,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import pe.edu.utp.animal_gym_api.common.response.ApiResponse;
 import pe.edu.utp.animal_gym_api.domain.auth.dto.AuthResponse;
 import pe.edu.utp.animal_gym_api.domain.auth.dto.LoginRequest;
@@ -19,9 +19,9 @@ import pe.edu.utp.animal_gym_api.domain.user.User;
 
 @RestController
 @RequestMapping("/api/auth")
+@RequiredArgsConstructor
 public class AuthController {
-	@Autowired
-	private AuthService authService;
+	private final AuthService authService;
 
 	@PostMapping("/login")
 	public ResponseEntity<ApiResponse<AuthResponse>> login(@Valid @RequestBody LoginRequest loginRequest) {
