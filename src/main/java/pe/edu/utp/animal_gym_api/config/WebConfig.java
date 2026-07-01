5219d79ecb634f7bc3ba8e59735efcecd7cdd6b9
aomine <aomine1745@gmail.com>
2026-07-01 01:41:35 -0500

    feat: agregar github action para ci

--- src/main/java/pe/edu/utp/animal_gym_api/config/WebConfig.java
+++ src/main/java/pe/edu/utp/animal_gym_api/config/WebConfig.java
@@ -1,10 +1,12 @@
 package pe.edu.utp.animal_gym_api.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@Profile("dev")
 public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
